658b8da6dbd0a008119d408d6736e134235406b0
ThatTMan <tpdimmock@gmail.com>
2026-02-22 10:59:20 -0800

    aegth

--- Game-Jam-p5/sketch.js
+++ Game-Jam-p5/sketch.js
@@ -1,28 +1,34 @@
-import { Player_Control } from "./player";
 
 createCanvas(700,1000);
-var speed = 40;
-let player = new Sprite(0,0,50,50,DYNAMIC);
-function setup() {
-}
+frameRate(60);
+
+var speed = 10;
+let player = new Sprite(350,800,50,50,DYNAMIC);
+let testObstical = new Sprite(350,100,50,50,KINEMATIC);
 
 function update() {
 	background('green');
+
 	Player_Control();
 
+    Scroll();
 }
 
 function Player_Control(){
-    if(keyDown("W")){
+    if(keyboard.pressing("W")){
         speed += 1;
     }
-    if(keyDown("S")){
+    if(keyboard.pressing("S")){
         speed -= 1;
     }
-    if(keyDown("A")){
-        player.velocityX=-5;
+    if(keyboard.pressing("A")){
+        player.position.x-=5;
+    } 
+    else if(keyboard.pressing("D")){
+        player.position.x+=5;
     }
-    if(keyDown("D")){
-        player.velocityX=5;
-    }
+}
+
+function Scroll(){
+    testObstical.position.y += speed;
 }
